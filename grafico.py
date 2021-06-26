34e8fba6f02171bc783360c7eb486f59792a2d7d
sd <silvia.donzelli@studenti.unipr.it>
2021-06-26 17:53:45 +0200

    comprensione grafico

--- grafico.py
+++ grafico.py
@@ -5,6 +5,8 @@ import pylab
 
 G = nx.DiGraph()
 
+#aggiungo archi con vari pesi
+#noi avremo aziende e correlazioni
 G.add_edges_from([('A', 'B'),('C','D'),('G','D')], weight=1) #aggiungi archi e dai un peso -> lo scrive 
 G.add_edges_from([('D','A'),('D','E'),('B','D'),('D','E')], weight=2)
 G.add_edges_from([('B','C'),('E','F')], weight=3)
@@ -23,17 +25,17 @@ edge_colors = ['black' if not edge in red_edges else 'red' for edge in G.edges()
 
 pos=nx.spring_layout(G)
 
-#aggiunte silvia
-options = {
+#aggiunte silvia: nodi tutti rossi con nomi 
+'''options = {
     'node_color': 'red',
     'node_size': 1000,
     'width': 3,
     'arrowstyle': '<->',
     'arrowsize': 12,
 }
-#nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
-#aggiunte silvia
-nx.draw_networkx(G, arrows=True, **options)
+nx.draw_networkx(G, arrows=True, **options)'''
 
-#nx.draw(G,pos, node_color = values, node_size=1500,edge_color=edge_colors,edge_cmap=plt.cm.Reds)
+#codice originale
+nx.draw_networkx_edge_labels(G,pos,edge_labels=edge_labels)
+nx.draw(G,pos, node_color = values, node_size=1500,edge_color=edge_colors,edge_cmap=plt.cm.Reds)
 pylab.show()
