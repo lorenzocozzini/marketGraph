b01b82d13686ac5c7840a196f05bc756881afade
giovannapalomba <giovanna.palomba24@gmail.com>
2021-06-27 12:52:10 +0200

    calcolo tutto + grafo

--- correlation.py
+++ correlation.py
@@ -13,7 +13,7 @@ import pylab
 from math import sqrt
 
 T = 10 #TODO leggi da args
-G = nx.DiGraph()
+G = nx.Graph()
 
 def get_adj_close(ticker):
     myclient = pymongo.MongoClient("mongodb://160.78.28.56:27017/") #160.78.28.56
@@ -29,7 +29,6 @@ def get_adj_close(ticker):
     for j in last_doc:
         adj_close.append(j["AdjClose"])
     return adj_close
-
 
 def get_correlation(adj_close_1, adj_close_2):
     #arg1
@@ -50,63 +49,72 @@ def get_correlation(adj_close_1, adj_close_2):
     arg3 = sum(r1_quad_sottr)/T
     arg4 = sum(r2_quad_sottr)/T
     
-    corr_mantegna = (arg1 -arg2)/sqrt(arg3*arg4)
+    if (sqrt(arg3*arg4) != 0):
+        corr_mantegna = (arg1 -arg2)/sqrt(arg3*arg4)
+    else:
+        corr_mantegna = 0 #indefinita
     return corr_mantegna
 
 def worker(list, return_list):
     id = int(multiprocessing.current_process().name)
-    print(list)
+    #print(list)
     num_records = int(len(list)/5)
     corr_list = []
+    #se non è l'ultimo 
+    if (id + 1 < 5):
+        last_index = num_records*(id+1)
+    #se è l'ultimo
+    else:
+         last_index = len(list)
     #scarica i dati e calcola correlazioni
-    for i in range(num_records*id, num_records*(id+1)):
-        print("Ticker 1: " + list[i])
+    for i in range(num_records*id, last_index):
+        #print("Ticker 1: " + list[i])
         adj_close_1 = get_adj_close(list[i])
-        print("adj_close_1", adj_close_1)
-        if (len(adj_close_1) == T):                 #calcolo correlazione solo se ho i dati per tutto l'intervallo
+        #print("adj_close_1", adj_close_1)
+        if (len(adj_close_1) == T and not(None in adj_close_1)):                 #calcolo correlazione solo se ho i dati per tutto l'intervallo e non ci sono dati null
             global G
             for j in range(i+1, len(list)):
-                print("Ticker 2: " + list[j])
+                #print("Ticker 2: " + list[j])
                 adj_close_2 = []
                 adj_close_2 = get_adj_close(list[j])
-                print("adj_close_2", adj_close_2)
-                if (len(adj_close_2) == T):          #calcolo correlazione solo se ho i dati per tutto l'intervallo
+                #print("adj_close_2 " + list[j], adj_close_2)
+                if (len(adj_close_2) == T and not(None in adj_close_2)):          #calcolo correlazione solo se ho i dati per tutto l'intervallo e non ci sono dati null
                     #calcola correlazione
-                    print("Correlation " + list[i] + " - " + list[j] + ":")
-                    correlation = np.corrcoef(adj_close_1, adj_close_2)
-                    print(correlation)
+                    #print("Correlation " + list[i] + " - " + list[j] + ":")
+                    #correlation = np.corrcoef(adj_close_1, adj_close_2)
+                    #print(correlation)
                     
                     corr_mantegna = get_correlation(adj_close_1, adj_close_2)
-                    print("Correlation " + list[i] + " - " + list[j] + ":")
-                    print(str(corr_mantegna))
+                    #print("Correlation " + list[i] + " - " + list[j] + ":")
+                    #print(str(corr_mantegna))
                     
                     #if -> mostro solo se correlazione > theta
                     #G.add_edges_from([(list[i], list[j])], weight=corr_mantegna)
                     if (corr_mantegna > 0.75):
-                        corr_list.append((list[i], list[j], corr_mantegna))
+                        corr_list.append((list[i], list[j], round(corr_mantegna, 3)))
                     
                 else:
-                    print("adj_close_2 non ha dati sufficienti")
+                    print(list[j] + " non ha dati sufficienti")
         else:
-            print("adj_close_1 non ha dati sufficienti")
+            print(list[i] + " non ha dati sufficienti")
         return_list[id] = corr_list
-    
-            
-    #print (multiprocessing.current_process().name," Worker")
     return
 
 if __name__ == '__main__':
     df = pd.read_csv('us_market.csv')
-    symbol_array = df["Symbol"].values
-    #message = json.dumps(symbol_array.tolist())
+    symbol_array = df["Symbol"].values  
+    
+    #il master ha già lista completa
+    #toglie lista di aziende che i nodi non hanno trovato
+    
+    
     adj_close = []
-    sub_list = symbol_array[0:16]
-    #print(sub_list)
+    #sub_list = symbol_array[0:16]
     jobs = []
     manager = multiprocessing.Manager()
     corr_list = manager.dict()
     for i in range(5):
-        p = multiprocessing.Process(name=str(i),target=worker, args=(sub_list, corr_list))
+        p = multiprocessing.Process(name=str(i),target=worker, args=(symbol_array, corr_list))
         jobs.append(p)
         p.start()
     
@@ -114,20 +122,15 @@ if __name__ == '__main__':
     for job in jobs:
         job.join()
     
-    print("Tutto finito")
-    #print(corr_list)
-    #G.add_edges_from([('C','F')], weight=0.214214)
+    print("Fine processing correlation")
+    #disegno grafo
     for i in range(5):
         for tupla in corr_list[i]:
             G.add_edges_from([(tupla[0],tupla[1])], weight=tupla[2])
     
-    
-    options = {
-        'node_color': 'red',
-        'node_size': 1000,
-        'width': 3,
-        'arrowstyle': '-',
-        'arrowsize': 0.2,
-    }
-    nx.draw_networkx(G, arrows=True, **options)
+    edge_labels=dict([((u,v,),d['weight'])
+                for u,v,d in G.edges(data=True)])
+    pos=nx.kamada_kawai_layout(G)
+    nx.draw_networkx_edge_labels(G,pos,edge_labels = edge_labels)
+    nx.draw(G,pos,with_labels = True, node_size=200, node_color = 'lightblue')
     pylab.show()
